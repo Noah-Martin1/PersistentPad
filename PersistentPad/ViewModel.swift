8da7693b4753bb43ede88c6790b3344b455ef97e
Noah Martin <u7124466@anu.edu.au>
2026-06-02 16:31:23 +1000

    added logic for auto save

--- PersistentPad/ViewModel.swift
+++ PersistentPad/ViewModel.swift
@@ -10,15 +10,28 @@ import Combine
 
 final class EditorViewModel: ObservableObject {
 
-    // MARK: - State (UI watches this)
+    // MARK: - State UI watches this
     @Published var text: String = ""
     @Published var fileURL: URL?
     @Published var showStartup: Bool = true
+    
+    private var cancellables = Set<AnyCancellable>()
+    
+    init() {
+        $text
+            .dropFirst() // avoid saving first event - being "" (text init)
+            .debounce(for: .milliseconds(500), scheduler: RunLoop.main)
+            .sink { [weak self] _ in
+                self?.save()
+            }
+            .store(in: &cancellables)
+    }
+
 
     // MARK: - Dependency
     private let fileService = FileService()
 
-    // MARK: - Actions (called by UI)
+    // MARK: - Actions called by UI
 
     func openFile() {
         guard let url = fileService.pickOpenFile() else { return }
@@ -42,4 +55,5 @@ final class EditorViewModel: ObservableObject {
         guard let url = fileURL else { return }
         fileService.save(text, to: url)
     }
+    
 }
